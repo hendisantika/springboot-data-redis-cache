32f3716256eef6ca1ca981ae9ab7883b183ba05c
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-24 05:01:13 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootdatarediscache/SpringbootDataRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdatarediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataRedisCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDataRedisCacheApplication.class, args);
+    }
+
+}
