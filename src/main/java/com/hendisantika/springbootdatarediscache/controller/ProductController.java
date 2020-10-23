d1c068341b18c26e090eb857d4d9dd5fb6408a19
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-24 05:18:25 +0700

    #7 - Add save & getAllProducts API in ProductController

--- src/main/java/com/hendisantika/springbootdatarediscache/controller/ProductController.java
+++ src/main/java/com/hendisantika/springbootdatarediscache/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootdatarediscache.controller;
 
+import com.hendisantika.springbootdatarediscache.entity.Product;
 import com.hendisantika.springbootdatarediscache.repository.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,13 @@ public class ProductController {
     @Autowired
     private ProductDao dao;
 
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return dao.findAll();
+    }
 }
