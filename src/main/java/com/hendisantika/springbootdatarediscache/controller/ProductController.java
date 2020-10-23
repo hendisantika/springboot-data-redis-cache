7b15c948127cf58a2ff97b59e40fed91b1777e88
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-24 05:19:02 +0700

    #8 - Add findProduct & remove API in ProductController

--- src/main/java/com/hendisantika/springbootdatarediscache/controller/ProductController.java
+++ src/main/java/com/hendisantika/springbootdatarediscache/controller/ProductController.java
@@ -3,8 +3,12 @@ package com.hendisantika.springbootdatarediscache.controller;
 import com.hendisantika.springbootdatarediscache.entity.Product;
 import com.hendisantika.springbootdatarediscache.repository.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +40,17 @@ public class ProductController {
     @GetMapping
     public List<Product> getAllProducts() {
         return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @Cacheable(key = "#id", value = "Product", unless = "#result.price > 1000")
+    public Product findProduct(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @CacheEvict(key = "#id", value = "Product")
+    public String remove(@PathVariable int id) {
+        return dao.deleteProduct(id);
     }
 }
