5a7cd571ff2fa521bc75822387f0e882d67178a8
hendisantika <hendisantika@yahoo.co.id>
2024-12-06 01:00:20 +0700

    Add TestController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootdatarediscache/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springbootdatarediscache.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-data-redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/12/24
+ * Time: 00.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class Controller {
+
+    private final CacheService redisCacheService;
+}
