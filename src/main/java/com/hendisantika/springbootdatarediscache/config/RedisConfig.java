a07e684ccdbe52afd11996f65b2f56d25539a1be
hendisantika <hendisantika@yahoo.co.id>
2024-12-06 00:55:11 +0700

    Add redisTemplate in RedisConfig🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootdatarediscache/config/RedisConfig.java
+++ src/main/java/com/hendisantika/springbootdatarediscache/config/RedisConfig.java
@@ -1,14 +1,23 @@
 package com.hendisantika.springbootdatarediscache.config;
 
+import com.hendisantika.springbootdatarediscache.entity.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,6 +41,25 @@ public class RedisConfig {
     @Value("${spring.data.redis.password}")
     private String password;
 
+    @Bean(name = "redisCacheManager")
+    CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory).cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                        .entryTtl(Duration.ofMinutes(30))).build();
+
+    }
+
+    @Bean
+    RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setConnectionFactory(connectionFactory);
+
+        return template;
+    }
+
     @Bean
     public JedisConnectionFactory connectionFactory() {
         RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
@@ -43,7 +71,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> template() {
+    public RedisTemplate<String, Object> redisConnectionFactory() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
@@ -53,5 +81,21 @@ public class RedisConfig {
         template.setEnableTransactionSupport(true);
         template.afterPropertiesSet();
         return template;
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory2() {
+        LettuceConnectionFactory lcf = new LettuceConnectionFactory();
+        lcf.setHostName(host);
+        lcf.setPort(6379);
+        lcf.afterPropertiesSet();
+        return lcf;
+    }
+
+    public RedisTemplate<String, User> redisTemplate() {
+        RedisTemplate<String, User> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory2());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
     }
 }
