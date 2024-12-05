a9395f3b498d987b6ecb40140d8697175a888688
hendisantika <hendisantika@yahoo.co.id>
2024-12-06 00:56:44 +0700

    Add CustomCacheResolver in RedisConfig🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootdatarediscache/config/CustomCacheResolver.java
@@ -0,0 +1,33 @@
+package com.hendisantika.springbootdatarediscache.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.CacheResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-data-redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/12/24
+ * Time: 00.56
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+public class CustomCacheResolver implements CacheResolver {
+
+    private static final int DEFAULT_TTL = 30;
+
+    private final CacheManager cacheManager;
+
+    private final Map<String, String> mapNameConfig = new HashMap<String, String>();
+}
