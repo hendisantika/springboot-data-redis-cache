12709b6d8f9ca6e9772874e2c205f33d88b1be36
hendisantika <hendisantika@yahoo.co.id>
2024-12-06 00:57:28 +0700

    Add resolveCaches in CustomCacheResolver🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootdatarediscache/config/CustomCacheResolver.java
+++ src/main/java/com/hendisantika/springbootdatarediscache/config/CustomCacheResolver.java
@@ -4,12 +4,19 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.cache.interceptor.CacheOperationInvocationContext;
 import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -36,5 +43,28 @@ public class CustomCacheResolver implements CacheResolver {
     public void init() {
         mapNameConfig.put("10m-", "10");
         mapNameConfig.put("20m-", "20");
+    }
+
+    @Override
+    public Collection<? extends Cache> resolveCaches(CacheOperationInvocationContext<?> context) {
+        log.warn("CustomCacheResolver resolveCaches");
+        if (context != null && context.getOperation() != null && (context.getOperation().getCacheNames() != null
+                && !context.getOperation().getCacheNames().isEmpty())) {
+            Collection<CustomCache> caches = new HashSet<>();
+
+            Set<String> cacheNames = context.getOperation().getCacheNames().stream().collect(Collectors.toSet());
+
+            cacheNames.forEach(cacheName -> {
+
+                Cache cache = cacheManager.getCache(cacheName);
+                CustomCache customCache = cacheWithCustomTTL(cacheName, cache);
+                caches.add(customCache);
+            });
+            return caches;
+
+        }
+        // this should not reach here
+        log.warn("CustomCacheResolver context is empty");
+        return Collections.emptyList();
     }
 }
