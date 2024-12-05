d0f4f07cfe98aa843dcf7e979ae110bc960ded1f
hendisantika <hendisantika@yahoo.co.id>
2024-12-06 01:01:19 +0700

    Add CacheService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/com/hendisantika/springbootdatarediscache/service/CacheService.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootdatarediscache.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-data-redis-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 06/12/24
+ * Time: 01.01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Slf4j
+public class CacheService {
+
+}
